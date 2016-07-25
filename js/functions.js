3edfb15cc404a6d75414bb0cb7d4c16492e94ffb
Jason Roy <Jason@Macbook-Pro.local>
2016-07-25 14:24:20 +0530

    Inline jQuery

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-var windowWidth = $(window).width();
-if ( windowWidth < 659 ) {
-    $('#viewMore').removeClass("hidden");
-    $('.next').hide();
-    $('#viewMore').click(function(){
-    $('.next').slideToggle();
-    $('#viewMore').html($('#viewMore').text() == 'View Less' ? 'View More' : 'View Less');
-    });
-}
